d275c3d3ac429e2615ba539d382acfdebee39012
IrinaMironova <irina250282@mail.ru>
2021-12-09 15:12:54 +0500

    HomeWork1

--- src/main/java/ru/geekbrains/spring/lesson1/WorkApp.java
+++ src/main/java/ru/geekbrains/spring/lesson1/WorkApp.java
@@ -1,4 +1,14 @@
 package ru.geekbrains.spring.lesson1;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class WorkApp {
+    public static void main(String[] args) {
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        Registration registration = context.getBean("registration", Registration.class);
+        registration.doVisit();
+    }
 }
